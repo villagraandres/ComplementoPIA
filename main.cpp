78ae2facf1cc8c8c14d2332c0bdbe88ee3bfacc4
Sans_Nrj <168495014+ArtERSON@users.noreply.github.com>
2024-11-20 12:13:39 -0600

    ya saquenme de progra estructurada con edna

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <regex>
 #include <cstring>
+
 using namespace std;
+
 bool buscarArchivo(char numeroVenta[100],FILE *);
 bool existeNumero(int *arrf,int,int );
-int main() {
+
+int main() 
+{
     char opcion;
     FILE *encabezadoVentas,*detalle_venta;
     char numeroVenta[100], nombre[80], fecha[50],descripcion[100],cantidad,continuar;
@@ -16,29 +20,36 @@ int main() {
     cout<<"c. Detalle por cliente"<<endl;
     cout<<"d. Salir"<<endl;
     cin>>opcion;
-    switch (opcion) {
+    switch (opcion) 
+    {
         case 'a':
-
-            try {
+            try 
+                {
                 encabezadoVentas=fopen("encabezado_venta.txt","a+");
                 detalle_venta=fopen("detalle_venta.txt.","a+");
-                if(detalle_venta==NULL || encabezadoVentas==NULL) {
+                if(detalle_venta==NULL || encabezadoVentas==NULL) 
+                {
                     throw "Error al crear el archivo";
-                }else {
-
-                    do {
+                }
+                else 
+                {
+                    do 
+                    {
                         cout<<"Ingresa el numero de venta"<<endl;
                         cin>>numeroVenta;
-                    }while(!regex_match(numeroVenta,patronVenta) or buscarArchivo(numeroVenta,encabezadoVentas));
+                    }
+                    while(!regex_match(numeroVenta,patronVenta) or buscarArchivo(numeroVenta,encabezadoVentas));
 
-                    do {
+                    do 
+                    {
                         cout<<"Ingrese la clave del cliente"<<endl;
                         cin>>claveCliente;
-                        if(claveCliente<100 or claveCliente>200) {
+                        if(claveCliente<100 or claveCliente>200) 
+                        {
                             cout<<"Clave invalida, debe estar entre 100 y 200"<<endl;
-
                         }
-                    }while(claveCliente<100 or claveCliente>200);
+                    }
+                    while(claveCliente<100 or claveCliente>200);
 
                     cout<<"Ingrese el nombre"<<endl;
                     cin.ignore();
@@ -46,46 +57,53 @@ int main() {
                     cout<<"Ingrese la fecha"<<endl;
                     cin.ignore();
                     cin.getline(fecha, sizeof(fecha));
-                    if(cin.fail()) {
+                    if(cin.fail()) 
+                    {
                         throw "Se han ingresado datos invalidos";
                     }
                     fprintf(encabezadoVentas,"%s %d |%s| %s\n",numeroVenta,claveCliente,nombre,fecha);
 
-                    do {
+                    do 
+                    {
+                        do 
+                        {
+                            cout<<"Ingrese la clave del articulo"<<endl;
+                            cin>>claveArticulo;
+                        }
+                        while(claveArticulo>100 or claveArticulo<1 or existeNumero(vistos,claveArticulo,consecutivo));
 
-
-                    do {
-                        cout<<"Ingrese la clave del articulo"<<endl;
-                        cin>>claveArticulo;
-                    }while(claveArticulo>100 or claveArticulo<1 or existeNumero(vistos,claveArticulo,consecutivo));
-
-                    cout<<"Ingrese la descripcion"<<endl;
-                    cin.ignore();
-                    cin.getline(descripcion, sizeof(descripcion));
-
-                    do {
-                        cout<<"Ingrese la cantidad deseada"<<endl;
-                        cin>>cantidad;
-                    }while(cantidad<0);
-
-                    do {
-                        cout<<"Ingrese el precio del articulo"<<endl;
+                        cout<<"Ingrese la descripcion"<<endl;
                         cin.ignore();
-                        cin>>precio;
-                    }while(precio<0);
-                    vistos[consecutivo]=claveArticulo;
+                        cin.getline(descripcion, sizeof(descripcion));
 
-                    fprintf(detalle_venta,"%s %d %d |%s| %d %f\n",numeroVenta,consecutivo,claveArticulo,descripcion,cantidad,precio);
-                    consecutivo++;
+                        do 
+                        {
+                            cout<<"Ingrese la cantidad deseada"<<endl;
+                            cin>>cantidad;
+                        }
+                            while(cantidad<0);
 
-                    cout<<"Desea continuar? S:si N:no";
-                    cin.ignore();
-                    cin>>continuar;
-                    }while(continuar!='N');
+                        do 
+                        {
+                            cout<<"Ingrese el precio del articulo"<<endl;
+                            cin.ignore();
+                            cin>>precio;
+                        }
+                        while(precio<0);
+                        vistos[consecutivo]=claveArticulo;
 
+                        fprintf(detalle_venta,"%s %d %d |%s| %d %f\n",numeroVenta,consecutivo,claveArticulo,descripcion,cantidad,precio);
+                        consecutivo++;
 
+                        cout<<"Desea continuar? S:si N:no";
+                        cin.ignore();
+                        cin>>continuar;
+                    }
+                    while(continuar!='N');
                 }
-            }catch (const char *e) {
+            }
+            catch (const char *e) 
+            {
                 cout<<e;
             }
             fclose(encabezadoVentas);
@@ -94,22 +112,28 @@ int main() {
     return 0;
 }
 
-bool existeNumero(int *arrf,int buscar,int tam ) {
+bool existeNumero(int *arrf,int buscar,int tam ) 
+{
     int i;
-        for(i=0;i<tam;i++) {
-        if (*(arrf + i) == buscar) {
-            return true;
+        for(i=0;i<tam;i++) 
+        {
+            if (*(arrf + i) == buscar) 
+            {
+                return true;
+            }
         }
-    }
     return false;
 }
 
-bool buscarArchivo(char numeroVentaf[100], FILE *archivo) {
+bool buscarArchivo(char numeroVentaf[100], FILE *archivo)
+{
     char numeroVenta[100], nombre[80], fecha[50];
     int claveCliente;
 
-    while(fscanf(archivo,"%s %d |%s| %s",&numeroVenta,&claveCliente,&nombre,&fecha)==4) {
-        if(strcmp(numeroVentaf,numeroVenta)==0) {
+    while(fscanf(archivo,"%s %d |%s| %s",&numeroVenta,&claveCliente,&nombre,&fecha)==4) 
+    {
+        if(strcmp(numeroVentaf,numeroVenta)==0) 
+        {
             return true;
         }
     }
